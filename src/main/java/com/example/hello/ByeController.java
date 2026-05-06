ced67023b43f8f02dbb24a8b9ba1fae638ac3411
mario <mario@mario-macui-MacBookPro.local>
2026-05-06 22:01:16 +0900

    Fix : class 이름 변경
    class 이름 변경

--- src/main/java/com/example/hello/ByeController.java
+++ src/main/java/com/example/hello/ByeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class byeController {
+public class ByeController {
     @GetMapping("/bye")
     public String sayBye(@RequestParam(defaultValue = "World") String name) {
         /*
