a9ad99c53cdcd935158c3354abf6364dc518ea6e
mario <mario@mario-macui-MacBookPro.local>
2026-05-06 23:22:20 +0900

    Fix: 위치 이동
    다음 실습 위해서 위치 디렉토리 안으로 위치 이동

--- src/main/java/com/example/hello/hello/HelloController.java
+++ src/main/java/com/example/hello/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.hello;
+package com.example.hello.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ src/main/java/com/example/hello/holiday/HolidayController.java
@@ -0,0 +1,8 @@
+package com.example.hello.holiday;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolidayController {
+    // TODO: 공휴일 체크 API
+}
